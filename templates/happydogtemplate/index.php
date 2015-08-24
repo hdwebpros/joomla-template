4a60b9b7509454964302a9a4b6db847c29a0d9b4
Ryan Boog <ryan@hdwebpros.com>
2015-08-24 10:51:20 -0500

    Let's remove the rest of the content and create them in modules instead.

--- templates/happydogtemplate/index.php
+++ templates/happydogtemplate/index.php
@@ -42,18 +42,12 @@ include_once JPATH_THEMES . '/' . $this->template . '/logic.php';
         </div>
         <!-- /.container-fluid -->
     </nav>
-
-    <header>
-        <div class="header-content">
-            <div class="header-content-inner">
-                <h1>Your Favorite Source of Free Bootstrap Themes</h1>
-                <hr>
-                <p>Start Bootstrap can help you build better websites using the Bootstrap CSS framework! Just download your template and start going, no strings attached!</p>
-                <a href="#about" class="btn btn-primary btn-xl page-scroll">Find Out More</a>
-            </div>
-        </div>
-    </header>
-
+    
+    <?php if ($this->countModules('header')) : ?>
+        <header>
+            <jdoc:include type="modules" name="header" style="none" />
+        </header>
+    <?php endif; ?>
     <jdoc:include type="modules" name="content-top" style="html5" />
 
     <section class="<?php echo $pageClass; ?>" id="<?php echo $view; ?>">
@@ -64,35 +58,6 @@ include_once JPATH_THEMES . '/' . $this->template . '/logic.php';
     </section>
 
     <jdoc:include type="modules" name="content-bottom" style="html5" />
-
-    <aside class="bg-dark">
-        <div class="container text-center">
-            <div class="call-to-action">
-                <h2>Free Download at Start Bootstrap!</h2>
-                <a href="#" class="btn btn-default btn-xl wow tada">Download Now!</a>
-            </div>
-        </div>
-    </aside>
-
-    <section id="contact">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8 col-lg-offset-2 text-center">
-                    <h2 class="section-heading">Let's Get In Touch!</h2>
-                    <hr class="primary">
-                    <p>Ready to start your next project with us? That's great! Give us a call or send us an email and we will get back to you as soon as possible!</p>
-                </div>
-                <div class="col-lg-4 col-lg-offset-2 text-center">
-                    <i class="fa fa-phone fa-3x wow bounceIn"></i>
-                    <p>123-456-6789</p>
-                </div>
-                <div class="col-lg-4 text-center">
-                    <i class="fa fa-envelope-o fa-3x wow bounceIn" data-wow-delay=".1s"></i>
-                    <p><a href="mailto:your-email@your-domain.com">feedback@startbootstrap.com</a></p>
-                </div>
-            </div>
-        </div>
-    </section>
 
     <!-- Plugin JavaScript -->
     <script src="<?php echo $this->baseurl; ?>/templates/<?php echo $this->template; ?>/js/jquery.easing.min.js"></script>
